cb5bd4acb1d93d852cb1e0dbc6e0a821515d4bdd
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-09-25 00:52:09 +0200

    Add favorite, ex.2

--- src/js/script.js
+++ src/js/script.js
@@ -8,30 +8,75 @@
     },
     containerOf: {
       booksList: '.books-list',
-    }
+      booksImage: '.books-list .book__image',
+    },
+
   };
   const templates = {
     bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
   };
 
-
+  // List of book rendering
   function render(){
 
-    for(let eachBook of dataSource.books){
+    /* find menu container */
+    const bookListContainer = document.querySelector(select.containerOf.booksList);
+      
+    for(const eachBook of dataSource.books){
     /* generate HTML based on template */
       const generatedHTML = templates.bookTemplate(eachBook);
       
       /* create element using utils.createElementFromHTML */
       const element = utils.createDOMFromHTML(generatedHTML);
 
-      /* find menu container */
-      const bookListContainer = document.querySelector(select.containerOf.booksList);
-
       /* add element to menu */
       bookListContainer.appendChild(element);
     }
   }
 
+  // Favorites
+  // pusta tablica favoriteBooks
+
+  const favoriteBooks = [];
+
+  function initActions(){
+
+    // reference to .book__image in .booksList
+    const booksImage = document.querySelectorAll(select.containerOf.booksImage);
+
+    // loop start
+    for(const image of booksImage){
+
+      // addEventListeners - dbl
+      image.addEventListener('dblclick', function(event){
+
+        // preventDefault
+        event.preventDefault();
+        const clickedElement = this;
+
+        //add class favorite 
+        clickedElement.classList.add('favorite');
+
+        // extract id from element
+        const bookId = clickedElement.getAttribute('data-id');
+
+        // add id faviriteBooks
+        favoriteBooks.push(bookId);
+        console.log('favoriteBooks:', favoriteBooks);
+      });
+    }
+  }
+
+
+
+
+
+
+
+
+
+
   render();
+  initActions();
 }
 
